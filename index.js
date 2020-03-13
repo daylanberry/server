c7c54d51115b604a1261715d017bf3911f2a2754
Daylan <daylanberry@gmail.com>
2020-03-12 22:58:38 -0700

    added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+require('dotenv').config()
 
 
 app.get('/', (req, res) => {
@@ -8,4 +9,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => console.log('connected to ' + PORT))
+app.listen(PORT, () => console.log('connected to ' + PORT))
